13fa01440670e26d6d16ac4dc7c0c5ff9188ac5c
kimlimjustin <kimlimjustin@gmail.com>
2020-12-17 20:05:58 +0700

    Added accordion feature

--- FireUI.js
+++ FireUI.js
@@ -75,4 +75,12 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         })
     })
+    document.querySelectorAll(".accordion").forEach(accordion => {
+        accordion.addEventListener("click", () => {
+            accordion.classList.toggle("accordion-active");
+            var panel = document.getElementById(accordion.dataset.panel);
+            if(panel.style.maxHeight) panel.style.maxHeight = null;
+            else panel.style.maxHeight = panel.scrollHeight + 'px';
+        })
+    })
 })
