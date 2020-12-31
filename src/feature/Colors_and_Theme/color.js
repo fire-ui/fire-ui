f4724a02059ec304eedcf91cfb9e84d9214ab92d
stanleyowen <69080584+stanleyowen@users.noreply.github.com>
2020-12-31 21:23:37 +0700

    Fixed #32

--- src/feature/Colors_and_Theme/color.js
+++ src/feature/Colors_and_Theme/color.js
@@ -2,16 +2,16 @@
 document.addEventListener("DOMContentLoaded", () => {
      // Set the website theme color according to the latest user preference saved in localStorage
      if(localStorage.getItem('theme-color')){
-        document.body.setAttribute('theme', localStorage.getItem('theme-color'))
+        document.body.setAttribute('data-theme', localStorage.getItem('theme-color'))
     }
     // Searching all attributes with switch-theme attribute
     document.querySelectorAll("[switch-theme]").forEach(btn => {
         btn.addEventListener("click", () => {
-            if(document.body.getAttribute('theme') === "dark"){
+            if(document.body.getAttribute('data-theme') === "dark"){
                 localStorage.setItem('theme-color', "light")
             }else{
                 localStorage.setItem('theme-color', "dark")
-            }document.body.setAttribute('theme', localStorage.getItem('theme-color'))
+            }document.body.setAttribute('data-theme', localStorage.getItem('theme-color'))
         })
     })
 })
