5918b7683679369d2bbe39417239857d77e4c1f4
kimlimjustin <kimlimjustin@gmail.com>
2021-01-16 14:42:44 +0700

    fix #46

--- FireUI.js
+++ FireUI.js
@@ -117,10 +117,10 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     })
     var marquee = document.querySelectorAll('marquee');
-    for(i=0; i<marquee.length; i++){
+    for(let i=0; i<marquee.length; i++){
         marquee[i].setAttribute('scrollamount', '10');
     }
-    for(x=0; x<marquee.length; x++){
+    for(let x=0; x<marquee.length; x++){
         if(marquee[x].hasAttribute('data-speed')){
             var speed = document.getElementsByTagName('marquee')[x].getAttribute('data-speed');
             document.getElementsByTagName('marquee')[x].setAttribute('scrollamount', speed);
