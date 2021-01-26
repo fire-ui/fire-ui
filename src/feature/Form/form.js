b8d814de26f532446e8cb1ce7bf916e0b62259be
kimlimjustin <kimlimjustin@gmail.com>
2021-01-26 20:41:00 +0700

    Added toggle switch feature

--- src/feature/Form/form.js
+++ src/feature/Form/form.js
@@ -5,4 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
             textarea.style.height = textarea.scrollHeight + 'px';
         })
     })
+    document.querySelectorAll(".toggle-switch").forEach(toggle => {
+        // Create a new element inside toggle input
+        let element = document.createElement('label');
+        element.classList.add('toggle-animation');
+        element.setAttribute("for", toggle.querySelector("input[type=checkbox]").getAttribute('id'));
+        toggle.appendChild(element);
+    })
 })
